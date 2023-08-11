7cf6fe290c6cc27656229b47d2b38192aa6cf6d8
Adrian Ariton <adrianariton@192-168-0-154.rdsnet.ro>
2023-08-11 15:35:35 +0300

    rethought entanglement gen

--- src/1_entangler.jl
+++ src/1_entangler.jl
@@ -1,23 +1,35 @@
 include("setup.jl")
 
 commtimes = [0.2, 0.14]
-sim, network = simulation_setup(8, 4, commtimes)
+sim, network = simulation_setup(4, 5, commtimes)
 node_timedelay = [0.4, 0.3]
 noisy_pair = noisy_pair_func(0.5)
-for (;src, dst) in edges(network)
-    chn = network[(src, dst), :channel]
-    @process sender_signalfreequbit(sim, network, chn[1], src, node_timedelay[1], node_timedelay[2])
-    @process sender_waitforfreequbitback(sim, network, chn[1], src, node_timedelay[1], node_timedelay[2])
-    @process receiver_findfreequbit(sim, network, chn[1], dst, node_timedelay[1], node_timedelay[2])
-    @process receiver_assignfoundqubit(sim, network, chn[1], dst, node_timedelay[1], node_timedelay[2])
+
+chn = network[(1, 2), :channel]
+for src in vertices(network)
+    @process signalfreequbit(sim, network, chn[1], src, node_timedelay[1], node_timedelay[2])
+    @process assignqubit(sim, network, chn[1], src, node_timedelay[1], node_timedelay[2])
+    @process waitandunlocklistener(sim, network, chn[1], src, node_timedelay[1], node_timedelay[2])
+
+    @process findfreequbitresp(sim, network, chn[1], src, node_timedelay[1], node_timedelay[2])
+    @process assignqubitback(sim, network, chn[1], src, node_timedelay[1], node_timedelay[2])
+
 end
 
+chn = network[(1, 2), :channel]
+for src in vertices(network)
+    @process purify_getready(sim, network, chn[1], src, node_timedelay[1], node_timedelay[2])
+    @process purify_confirm(sim, network, chn[1], src, node_timedelay[1], node_timedelay[2])
+    @process purify_expect(sim, network, chn[1], src, node_timedelay[1], node_timedelay[2])
+end
 
-run(sim, 10.)
+run(sim, 20.3)
 
 println()
+println("Reuslts: \n")
 for item in entanglement_status
-    print(item.first)
+    print(RED_FG("id:$(item.first)"))
     print(" \t: ")
-    println(item.second)
+    statusinfo(item.second)
+    println()
 end
